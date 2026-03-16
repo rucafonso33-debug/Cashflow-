eb3b85aca21b317114c16049aed688d5ba32ce5c
rucafonso33-debug <rucafonso33@gmail.com>
2026-03-16 20:22:38 +0000

    Rename app to Provera

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.futureflow.app',
-  appName: 'FutureFlow',
+  appId: 'com.provera.finance',
+  appName: 'Provera',
   webDir: 'dist'
 };
 
