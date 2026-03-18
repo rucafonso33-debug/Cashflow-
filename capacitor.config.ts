581e44c315bcc6bc8cebdbbf5c9ca0ff065df4e6
rucafonso33-debug <rucafonso33@gmail.com>
2026-03-18 22:59:35 +0000

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.provera.app',
   appName: 'Provera',
-  webDir: 'dist'
+  webDir: 'dist',
+
+  server: {
+    hostname: 'provera-finance.vercel.app',
+    androidScheme: 'https'
+  }
 };
 
 export default config;
